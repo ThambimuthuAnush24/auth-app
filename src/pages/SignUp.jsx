a908e7c635e6bd7cf5b37c2d51a54b8a70034371
ThambimuthuAnush24 <thambimuthuanush@gmail.com>
2025-10-17 22:16:19 +0530

    v

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -115,9 +115,9 @@ const SignUp = () => {
           
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">
-              Already have an account?{' '}
+              Already have an account?{''}
               <span className="font-medium text-blue-600 hover:text-blue-500 cursor-pointer">
-                Sign in
+                Sign  in
               </span>
             </p>
           </div>
